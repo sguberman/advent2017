44754df01cbd7b34662da8b2bc8ccb9e0f66d181
Seth Guberman <sguberman@gmail.com>
2017-12-01 09:23:18 -0500

    day01 captcha() passes tests

--- day01/day01.py
+++ day01/day01.py
@@ -6,8 +6,9 @@ Created on Fri Dec  1 08:58:50 2017
 """
 
 
-def captcha(number):
-    raise NotImplementedError
+def captcha(digits):
+    neighbors = digits[-1] + digits[:-1]
+    return sum(int(d) for d, n in zip(digits, neighbors) if d == n)
 
 
 if __name__ == '__main__':
