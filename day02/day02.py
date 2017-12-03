e58b9a466b8362b2be166492ca985bb7c7c97322
Seth Guberman <sguberman@gmail.com>
2017-12-02 21:00:41 -0500

    add main2 for part2 and update checksum to accept different row functions

--- day02/day02.py
+++ day02/day02.py
@@ -11,8 +11,8 @@ def rowdiff(row):
     return max(row) - min(row)
 
 
-def checksum(spreadsheet):
-    return sum(rowdiff(row) for row in spreadsheet)
+def checksum(spreadsheet, func=rowdiff):
+    return sum(func(row) for row in spreadsheet)
 
 
 def read_spreadsheet(filename):
@@ -34,6 +34,10 @@ def rowdiv(row):
         for b in row[:i] + row[i+1:]:
             if a % b == 0:
                 return a // b
+
+
+def main2(filename):
+    return checksum(read_spreadsheet(filename), func=rowdiv)
 
 
 if __name__ == '__main__':
--- day02/test_day02.py
+++ day02/test_day02.py
@@ -9,7 +9,7 @@ Created on Sat Dec  2 20:13:58 2017
 
 import pytest
 
-from day02 import rowdiff, checksum, read_spreadsheet, main, rowdiv
+from day02 import rowdiff, checksum, read_spreadsheet, main, rowdiv, main2
 
 
 def test_rowdiff():
@@ -36,6 +36,10 @@ def test_rowdiv():
     assert rowdiv([5, 9, 2, 8]) == 4
     assert rowdiv([9, 4, 7, 3]) == 3
     assert rowdiv([3, 8, 6, 5]) == 2
+
+
+def test_main2():
+    assert main2('test_input2.txt') == 9
 
 
 if __name__ == '__main__':
