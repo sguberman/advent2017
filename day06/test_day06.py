140f9a76dd27dab8e5e024dd183deee016e33bc3
Seth Guberman <sguberman@gmail.com>
2017-12-06 15:47:53 -0500

    add specification test for read_input()

--- day06/test_day06.py
+++ day06/test_day06.py
@@ -9,7 +9,7 @@ Created on Wed Dec  6 14:50:30 2017
 
 import pytest
 
-from day06 import reallocate, detect_loop, solution
+from day06 import reallocate, detect_loop, solution, read_input
 
 
 testdata = [((0, 2, 7, 0), (2, 4, 1, 2)),
@@ -30,6 +30,13 @@ def test_detect_loop():
 
 def test_solution():
     assert solution() == (5042, 1086)
+
+
+def test_read_input():
+    memory = read_input('input.txt')
+    assert isinstance(memory, tuple)
+    assert len(memory) == 16
+    assert all(isinstance(x, int) for x in memory)
 
 
 if __name__ == '__main__':
