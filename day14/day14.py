a40e70997d583c87964d9e4e0aca7fa7f1a702bf
Seth Guberman <sguberman@gmail.com>
2017-12-14 16:28:41 -0500

    start part2

--- day14/day14.py
+++ day14/day14.py
@@ -20,6 +20,37 @@ def part1(inp):
     return total, grid
 
 
+def part2(grid):
+
+    # First start with a region for each used square in the grid.
+    # Each region is a set with the (i, j) location of the used square
+    # and its neighbors (left/right and up/down).
+    regions = set()
+    for i, row in enumerate(grid):
+        for j, x in enumerate(row):
+            if x == '1':
+                region = frozenset({(i, j),
+                                    (i + 1, j), (i - 1, j),
+                                    (i, j + 1), (i, j - 1)})
+                regions.add(region)
+
+    # Now go through the list of regions and combine intersecting sets.
+    # Keep doing that until the list doesn't get any smaller.
+    prev_len = len(regions) + 1
+    while len(regions) != prev_len:
+        prev_len = len(regions)
+        combined = set()
+        region = regions.pop()
+        for other in regions:
+            if not region.isdisjoint(other):
+                combined.add(region)
+                combined.add(other)
+                new_regions.add(frozenset(region | other)
+        regions -= combined
+        regions += new_regions
+
+
+
 total, grid = part1('jzgqcdpd')
 print(total)
 for row in grid[:8]:
